956bbf1e7880eabefc77fe93eaa2b8c466dc1133
邪恶小法师 <92067197+Q1ngyuC@users.noreply.github.com>
2022-11-03 17:49:29 +0800

    头文件罢了

---
+++ ADRC.h
@@ -0,0 +1,25 @@
+#ifndef __ADRC_H__
+#define __ADRC_H__
+#include <stdint.h>
+#include "Ano_Math.h"
+#include "Math.h"
+//#define h 0;
+//#define h0 0;
+//#define r 0;
+//#define bata1 0;
+//#define bata2 0;
+//#define bata3 0;
+
+
+typedef struct ADRC
+{
+	float Z1,Z2,Z3,e,e1,e2,u,u0,v1,v2,v;
+
+	
+} _ADRC_;
+
+void ADRC_Init(_ADRC_ *sptr);
+float ADRC(float vin,float y , _ADRC_ *sptr);
+
+
+#endif
